81c8a5c02829d82ab76e1cc737efb49aadc22ac4
xumingzhi9 <827668037@qq.com>
2020-03-17 16:27:57 +0800

    delete dest

--- dest/es/input/style/index.js
+++
@@ -1,7 +0,0 @@
-/**
-* @file css入口
-*/
-import '../../core/styles/index.less';
-import './index.less';
-
-import '../../button/style';
--- dest/lib/row/style/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-require("../../core/styles/index");
-
-require("../../grid/style");
