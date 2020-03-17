81a9da777f260e6978aa9ccc257bbcac3ea03d72
xumingzhi9 <827668037@qq.com>
2020-03-17 16:37:43 +0800

    update release

--- release.sh
+++ release.sh
@@ -1,10 +1,13 @@
+rm -rf dest
+git add dest
+git commit -m 'delete dest'
 current_git_branch_pre_id=`git rev-parse HEAD`
 npm install
 npm run release
 git add dest
 git commit -m 'deloy'
 current_git_branch_latest_id=`git rev-parse HEAD`
-git checkout -b tempbranch
+git checkout -b tempbranch --track origin/cus_release
 git cherry-pick $current_git_branch_latest_id
 git push origin tempbranch:refs/for/cus_release
 git checkout c_release
