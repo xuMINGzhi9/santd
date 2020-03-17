a518f7be9ffb273f2bc994e7f76cb3b29c1b5340
xumingzhi9 <827668037@qq.com>
2020-03-17 16:38:19 +0800

    update release

--- release.sh
+++ release.sh
@@ -11,5 +11,5 @@ git checkout -b tempbranch --track origin/cus_release
 git cherry-pick $current_git_branch_latest_id
 git push origin tempbranch:refs/for/cus_release
 git checkout c_release
-git reset current_git_branch_pre_id --hard
+git reset $current_git_branch_pre_id --hard
 git branch -D tempbranch
