e04ba6543952bc7ad8ea50cf46251467ac43770d
xumingzhi9 <827668037@qq.com>
2020-04-19 20:01:28 +0800

    deloy

--- dest/es/date-picker/style/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * @file 组件样式入口
- */
-import './index.less';
-
-import '../../input/style';
-import '../../timepicker/style';
-import '../../tag/style';
--- dest/es/tree/style/index.js
+++
@@ -1,8 +0,0 @@
-/**
-* @file tree组件入口文件
-*/
-import '../../core/styles/index.less';
-import './index.less';
-
-// style dependencies
-import '../../checkbox/style';
