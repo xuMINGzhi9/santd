4efebcbcbc1cfb0304bcb7e35fbd4f010afd56b7
xumingzhi9 <827668037@qq.com>
2020-04-19 22:50:59 +0800

    delete dest

--- dest/es/tree-select/style/index.js
+++
@@ -1,8 +0,0 @@
-/**
-* @file css入口文件
-*/
-import '../../core/styles/index.less';
-import './index.less';
-
-import '../../select/style';
-import '../../checkbox/style';
--- dest/lib/input/style/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-require("../../core/styles/index.less");
-
-require("./index.less");
-
-require("../../button/style");
